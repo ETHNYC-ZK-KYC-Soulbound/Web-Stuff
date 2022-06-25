568ed7cc2853a91c9efb73c2144ecc48cdefb1a3
Devlyn Dorfer <devlyndorfer@gmail.com>
2022-06-25 12:08:37 -0400

    adapt stripe api from other repo to run serverlessly

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  const { name } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+  response.status(200).send("Hello");
 };
